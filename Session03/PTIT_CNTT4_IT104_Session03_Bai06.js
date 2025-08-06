f64063aa0dd32143e259d6838e5c943d15081f4a
TMon283 <lemonboy2k6@gmail.com>
2025-08-07 05:32:39 +0700

    update

--- Session03/PTIT_CNTT4_IT104_Session03_Bai06.js
+++ Session03/PTIT_CNTT4_IT104_Session03_Bai06.js
@@ -1,8 +1,8 @@
-let num1 = 12;
-let num2 = 2;
-let num3;
-let num4 = "10";
-let num5 = true;
+var num1 = 12;
+var num2 = 2;
+var num3;
+var num4 = "10";
+var num5 = true;
 num3 = num1 + num2;
 console.log(num3);
 num3 = num1 - num2;
@@ -11,7 +11,7 @@ num3 = num1 * num2;
 console.log(num3);
 num3 = num1 / num2;
 console.log(num3);
-num3 = num4 + num5;
-console.log(num3);
+var result = num4 + num5;
+console.log(result);
 // JS: ép kiểu tự động true -> "true" -> output: "10true"
 // TS: báo lỗi vì không thể cộng string với boolean
--- Session03/PTIT_CNTT4_IT104_Session03_Bai06.ts
+++ Session03/PTIT_CNTT4_IT104_Session03_Bai06.ts
@@ -13,8 +13,7 @@ console.log(num3);
 num3 = num1 / num2;
 console.log(num3);
 
-num3 = num4 + num5;
-console.log(num3);
+let result = num4 + num5;
+console.log(result);
 
-// JS: ép kiểu tự động true -> "true" -> output: "10true"
-// TS: báo lỗi vì không thể cộng string với boolean
+// ép kiểu tự động true -> "true" -> output: "10true"
